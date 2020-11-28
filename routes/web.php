22345f7e5023b99b08d51f3fee2b13bd3da92d28
Walter Junior <wpmj1277@gmail.com>
2020-11-28 17:21:18 -0300

    Create Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->group(['prefix' => 'users'], function () use ($router) {
+        $router->get('', 'UserController@index');
+        $router->post('', 'UserController@store');
+        $router->get('{id}', 'UserController@show');
+        $router->put('{id}', 'UserController@update');
+        $router->delete('{id}', 'UserController@destroy');
+    });
+
+    $router->group(['prefix' => 'equipments'], function () use ($router) {
+        $router->get('', 'EquipmentController@index');
+        $router->post('', 'EquipmentController@store');
+        $router->get('{id}', 'EquipmentController@show');
+        $router->put('{id}', 'EquipmentController@update');
+        $router->delete('{id}', 'EquipmentController@destroy');
+    });
+
+    $router->group(['prefix' => 'maintenance'], function () use ($router) {
+        $router->get('', 'MaintenanceController@index');
+        $router->post('', 'MaintenanceController@store');
+        $router->get('{id}', 'MaintenanceController@show');
+        $router->put('{id}', 'MaintenanceController@update');
+        $router->delete('{id}', 'MaintenanceController@destroy');
+    });
+});
