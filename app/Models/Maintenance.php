357daad3224a83b63f10405867fb2b7abc7e6eab
Walter Junior <wpmj1277@gmail.com>
2020-11-28 17:49:47 -0300

    Start HATEOAS

--- app/Models/Maintenance.php
+++ app/Models/Maintenance.php
@@ -8,6 +8,7 @@ class Maintenance extends Model
 {
     protected array $fillable = ['start', 'end', 'description', 'equipment_id', 'user_id'];
     protected $perPage = 10;
+    protected $appends = ['links'];
 
     public function user()
     {
@@ -17,5 +18,26 @@ class Maintenance extends Model
     public function equipment()
     {
         return $this->hasOne(Equipment::class);
+    }
+
+    public function getUserAttribute()
+    {
+        $user = User::find($this->user_id);
+        return $user->name;
+    }
+
+    public function getEquipmentAttribute()
+    {
+        $equipment = Equipment::find($this->equipment_id);
+        return $equipment->name;
+    }
+
+    public function getLinksAttribute(): array
+    {
+        return [
+            'self' => '/api/maintenance/' . $this->id,
+            'equipment' => '/api/equipment/' . $this->equipment_id,
+            'user' => '/api/user/' . $this->user_id
+        ];
     }
 }
