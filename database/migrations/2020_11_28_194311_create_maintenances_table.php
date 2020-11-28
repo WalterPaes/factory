1f181ad871750ba80e26a87a9796750e27517e85
Walter Junior <wpmj1277@gmail.com>
2020-11-28 16:48:46 -0300

    Create Migrations

---
+++ database/migrations/2020_11_28_194311_create_maintenances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaintenancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenances', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->string('description');
+            $table->unsignedBigInteger('equipment_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('equipment_id')->references('id')->on('equipments');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenances');
+    }
+}
