fdd388205a41bbc2b4965600e69cb22c431bac62
Walter Junior <wpmj1277@gmail.com>
2020-11-28 16:52:12 -0300

    Create BaseController

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Client\Request;
+
+abstract class BaseController extends Controller
+{
+    protected string $model;
+
+    public function index(Request $request)
+    {
+        return $this->model::paginate($request->per_page);
+    }
+
+    public function show(int $id)
+    {
+        $data = $this->model::find($id);
+        if (is_null($data)) {
+            return response()
+                ->json('', 404);
+        }
+        return $data;
+    }
+
+    public function destroy(int $id)
+    {
+        $total = $this->model::destroy($id);
+        if ($total < 1) {
+            return response()->json('Resource not found', 404);
+        }
+        return response()->json('', 204);
+    }
+
+    abstract function store(Request $request);
+
+    abstract function update(int $id, Request $request);
+}
+
