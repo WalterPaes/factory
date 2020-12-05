ca4e2f80c5f75ba913ac2d48445f16601ca7e83c
Walter Junior <wpmj1277@gmail.com>
2020-12-05 12:22:40 -0300

    Resolving cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,7 +19,7 @@ class CorsMiddleware
             'Access-Control-Allow-Methods'     => 'POST, GET, PUT, DELETE, OPTIONS',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, Access-Control-Allow-Headers'
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
 
         if ($request->isMethod('OPTIONS'))
