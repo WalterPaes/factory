dd1238630feef30a838b9f11c5b10cdaf9142b68
WalterPaes <wpmj1277@gmail.com>
2022-05-13 20:05:08 -0300

    Enable cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -60,6 +60,7 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('cors');
 
 /*
 |--------------------------------------------------------------------------
@@ -73,7 +74,7 @@ $app->configure('app');
 */
 
  $app->middleware([
-    App\Http\Middleware\CorsMiddleware::class
+    Fruitcake\Cors\HandleCors::class,
  ]);
 
  $app->routeMiddleware([
@@ -93,6 +94,7 @@ $app->configure('app');
 
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Fruitcake\Cors\CorsServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
