79741df5a163ebc6765b23e35a98ff56b93dd136
WalterPaes <wpmj1277@gmail.com>
2022-05-13 19:15:38 -0300

    Create Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+
+        User::create([
+            'name' => 'Walter Paes',
+            'username' => 'wpaes',
+            'password' => Hash::make('roinuj'),
+            'status' => 1
+        ]);
+
+        User::create([
+            'name' => 'Walter Junior',
+            'username' => 'wjunior',
+            'password' => Hash::make('roinuj'),
+            'status' => 1
+        ]);
     }
 }
